24890632482ef2e883cbd5dd2ac59916de2a1f7a
parassingh898 <psingh@finopsys.ai>
2024-11-05 10:38:31 +0530

    code merged sucessfully for tooltip

--- src/AccountPayable/AQ/AQ.js
+++ src/AccountPayable/AQ/AQ.js
@@ -20,6 +20,8 @@ import PreviewSection from './PreviewSection/PreviewSection';
 import EditIcon from '@mui/icons-material/Edit';
 import DeclineTable from './DeclineTable/DeclineTable';
 import FilterDrawer from './FilterSection/FilterDrawer';
+import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
 
 const MAX_FILE_SIZE = 500 * 1024 * 1024;
 
@@ -58,6 +60,16 @@ function AQ() {
     amount: { equalTo: "", greaterThan: "", lessThan: "" },
     selectedMethods: [],
     selectedDepartments: [],});
+    const [showVendorNameSearch, setShowVendorNameSearch] = useState(false);
+    const [searchQueryByName, setSearchQueryByName] = useState('');
+    const [selectedVendorName, setSelectedVendorName] = useState('');
+    const [showCrossIcon, setShowCrossIcon] = useState(false);
+    const [showDropdown, setShowDropdown] = useState(false);
+    const [showBillNumberSearch, setShowBillNumberSearch] = useState(false);
+    const [searchQueryByBillNumber, setSearchQueryByBillNumber] = useState('');
+    const [selectedVendorBillNumber, setSelectedVendorBillNumber] = useState('');
+    const [showCrossBillNumber, setShowCrossBillNumber] = useState(false);
+    const [showDropdownBillNumber, setShowDropdownBillNumber] = useState(false);
   
   let index="";
     // Fetch invoices from the backend
@@ -119,7 +131,71 @@ function AQ() {
       setFilteredData(tempData);
     }, [filters, invoices]);
 
-
+    //tooltip
+    const handleMouseEnter = () => {
+      setShowVendorNameSearch(true);  // Show search bar on hover
+    };
+    const handleMouseLeave = () => {
+      setShowVendorNameSearch(false);  // Hide search bar when not hovered
+    };
+    const handleNameSearchChange = (e) => {
+      setSearchQueryByName(e.target.value);
+      setShowDropdown(e.target.value.length > 0);
+      setShowCrossIcon(false); // Reset cross icon when typing
+    };
+    const handleVendorClick = (vendorName) => {
+      setSelectedVendorName(vendorName);
+      setSearchQueryByName(vendorName); // Set input value to selected vendor
+      setShowCrossIcon(true); 
+      setShowDropdown(false);
+      
+      const filtered = invoices.filter(invoice => invoice.vendorName.toLowerCase() === vendorName.toLowerCase());
+      setFilteredData(filtered); 
+        // setInvoices(filtered)
+    };
+    const clearSearch = () => {
+      setSearchQueryByName('');
+      setSelectedVendorName(''); // Clear selected vendor
+      setShowCrossIcon(false); // Hide cross icon
+      setShowDropdown(false);
+      setFilteredData(invoices);
+    };
+    const filteredVendorNameInvoices = invoices.filter(invoice =>
+      invoice.vendorName.toLowerCase().includes(searchQueryByName.toLowerCase())
+    );
+    // ------------------------toottip bill numbber---------------------
+    const handleMouseEnterBill = () => {
+      setShowBillNumberSearch(true);  // Show search bar on hover
+    };
+    const handleMouseLeaveBill = () => {
+      setShowBillNumberSearch(false);  // Hide search bar when not hovered
+    };
+    const handleBillNumberSearch = (e) => {
+      setSearchQueryByBillNumber(e.target.value);
+      setShowDropdownBillNumber(e.target.value.length > 0);
+      setShowCrossBillNumber(false); // Reset cross icon when typing
+    };
+    const handleBillNumber = (billId) => {
+      setSelectedVendorBillNumber(billId);
+      setSearchQueryByBillNumber(billId); // Set input value to selected vendor
+      setShowCrossBillNumber(true); 
+      setShowDropdownBillNumber(false);
+      
+      const filtered = invoices.filter(invoice => invoice.billId?.toLowerCase() === billId?.toLowerCase());
+      setFilteredData(filtered); 
+        // setInvoices(filtered)
+    };
+    const clearBillNumber = () => {
+      setSearchQueryByBillNumber('');
+      setSelectedVendorBillNumber(''); // Clear selected vendor
+      setShowCrossBillNumber(false); // Hide cross icon
+      setShowDropdownBillNumber(false);
+      setFilteredData(invoices);
+    };
+    const filteredBillNumberInvoices = invoices.filter(invoice =>
+      invoice.billId.toLowerCase().includes(searchQueryByBillNumber.toLowerCase())
+    );
+      
     // --------------------------------preview-----------------------------------
     const handleShowPreview = (invoice, index) =>{ 
       setShowPreview(true);
@@ -390,11 +466,81 @@ const handleAccept = async (index) => {
               <Table className="custom-width">
                 <thead>
                   <tr>
-                    <th>
-                      <input type="checkbox" />
+                  <th  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} ondata-toggle="tooltip" data-placement="bottom" >
+                    <input type="checkbox"  />
                       &nbsp;&nbsp;&nbsp;Vendor Name
+                      {
+                               showVendorNameSearch 
+                                       && 
+                      <div className="vendorNameTooltip">
+                        <div className="vendorNameSearchDiv">
+                          <input id="vendorNameSearch"  type="text"  className="form-control" placeholder={selectedVendorName || "Search Vendor"} // Update placeholder
+                              value={searchQueryByName} onChange={handleNameSearchChange}/>
+                          {
+                            showCrossIcon 
+                                   ? 
+                              <CloseIcon
+                                style={{  fontSize: '19px',  position: 'absolute', top: '12px',left: '79%', color: 'black', cursor: 'pointer', }}  onClick={clearSearch}/>
+                                  : 
+                              <SearchIcon
+                                style={{ fontSize: '19px', position: 'absolute', top: '12px', left: '79%',color: 'black', }}/>
+                            }
+                          {
+                             showDropdown && searchQueryByName
+                                       ? 
+                              filteredVendorNameInvoices.length > 0 
+                                          ? 
+                              filteredVendorNameInvoices.map((invoice) => (
+                                <div key={invoice.id} className="invoiceCard"  onClick={() => handleVendorClick(invoice.vendorName)} >
+                                  <div className="vendorName" id='filterTooltip'>{invoice.vendorName}</div>
+                                </div>
+                              ))
+                                              : 
+                            <div className="noResults"  id='filterTooltip'> No matching invoices found</div>
+                                                   : 
+                                                ""
+                          }
+                        </div>
+                      </div>
+                    }
                     </th>
-                    <th>Bill Number</th>
+                    <th  onMouseEnter={handleMouseEnterBill} onMouseLeave={handleMouseLeaveBill} ondata-toggle="tooltip" data-placement="bottom" >
+                      Bill Number
+                     {
+                               showBillNumberSearch 
+                                       && 
+                      <div className="vendorNameTooltip" id='billNumberAQTool'>
+                        <div className="vendorNameSearchDiv">
+                          <input id="vendorNameSearch"  type="text"  className="form-control" placeholder={selectedVendorBillNumber || "Search Vendor"} // Update placeholder
+                              value={searchQueryByBillNumber} onChange={handleBillNumberSearch}/>
+                          {
+                            showCrossBillNumber 
+                                   ? 
+                              <CloseIcon
+                                style={{  fontSize: '19px',  position: 'absolute', top: '12px',left: '79%', color: 'black', cursor: 'pointer', }}  onClick={clearBillNumber}/>
+                                  : 
+                              <SearchIcon
+                                style={{ fontSize: '19px', position: 'absolute', top: '12px', left: '79%',color: 'black', }}/>
+                            }
+                          {
+                             showDropdownBillNumber && searchQueryByBillNumber
+                                       ? 
+                              filteredBillNumberInvoices.length > 0 
+                                          ? 
+                              filteredBillNumberInvoices.map((invoice) => (
+                                <div key={invoice.id} className="invoiceCard"  onClick={() => handleBillNumber(invoice.billId)} >
+                                  <div className="vendorName" id='filterTooltip'>{invoice.billId}</div>
+                                </div>
+                              ))
+                                              : 
+                            <div className="noResults"  id='filterTooltip'>No matching invoices found</div>
+                                                   : 
+                                                ""
+                          }
+                        </div>
+                      </div>
+                    }
+                    </th>
                     <th>Bill Date</th>
                     <th>Inbox Method</th>
                     <th>Amount</th>
@@ -443,8 +589,81 @@ const handleAccept = async (index) => {
                 <thead>
                   <tr>
                     <th> <input type="checkbox" /> &nbsp;&nbsp;&nbsp;Vendor Name </th>
-                    <th>Bill Number</th>
-                    <th>Bill Date</th>
+                    <th  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} ondata-toggle="tooltip" data-placement="bottom" >
+                      <input type="checkbox"  />
+                      &nbsp;&nbsp;&nbsp;Vendor Name
+                     {
+                               showVendorNameSearch 
+                                       && 
+                      <div className="vendorNameTooltip" style={{ width: '12%'}}> 
+                        <div className="vendorNameSearchDiv">
+                          <input id="vendorNameSearch"  type="text"  className="form-control" placeholder={selectedVendorName || "Search Vendor"} // Update placeholder
+                              value={searchQueryByName} onChange={handleNameSearchChange}/>
+                          {
+                            showCrossIcon 
+                                   ? 
+                              <CloseIcon
+                                style={{  fontSize: '19px',  position: 'absolute', top: '12px',left: '79%', color: 'black', cursor: 'pointer', }}  onClick={clearSearch}/>
+                                  : 
+                              <SearchIcon
+                                style={{ fontSize: '19px', position: 'absolute', top: '12px', left: '79%',color: 'black', }}/>
+                            }
+                          {
+                             showDropdown && searchQueryByName
+                                       ? 
+                              filteredVendorNameInvoices.length > 0 
+                                          ? 
+                              filteredVendorNameInvoices.map((invoice) => (
+                                <div key={invoice.id} className="invoiceCard"  onClick={() => handleVendorClick(invoice.vendorName)} >
+                                  <div className="vendorName" id='filterTooltip'>{invoice.vendorName}</div>
+                                </div>
+                              ))
+                                              : 
+                            <div className="noResults"  id='filterTooltip'>No matching invoices found</div>
+                                                   : 
+                                                ""
+                          }
+                        </div>
+                      </div>
+                    }
+                    </th>
+                    <th  onMouseEnter={handleMouseEnterBill} onMouseLeave={handleMouseLeaveBill} ondata-toggle="tooltip" data-placement="bottom" >
+                      Bill Number
+                     {
+                               showBillNumberSearch 
+                                       && 
+                      <div className="vendorNameTooltip"  style={{ width: '9%'}}>
+                        <div className="vendorNameSearchDiv">
+                          <input id="vendorNameSearch"  type="text"  className="form-control" placeholder={selectedVendorBillNumber || "Search Vendor"} // Update placeholder
+                              value={searchQueryByBillNumber} onChange={handleBillNumberSearch}/>
+                          {
+                            showCrossBillNumber 
+                                   ? 
+                              <CloseIcon
+                                style={{  fontSize: '19px',  position: 'absolute', top: '12px',left: '79%', color: 'black', cursor: 'pointer', }}  onClick={clearBillNumber}/>
+                                  : 
+                              <SearchIcon
+                                style={{ fontSize: '19px', position: 'absolute', top: '12px', left: '79%',color: 'black', }}/>
+                            }
+                          {
+                             showDropdownBillNumber && searchQueryByBillNumber
+                                       ? 
+                              filteredBillNumberInvoices.length > 0 
+                                          ? 
+                              filteredBillNumberInvoices.map((invoice) => (
+                                <div key={invoice.id} className="invoiceCard"  onClick={() => handleBillNumber(invoice.billId)} >
+                                  <div className="vendorName" id='filterTooltip'>{invoice.billId}</div>
+                                </div>
+                              ))
+                                              : 
+                            <div className="noResults">No matching invoices found</div>
+                                                   : 
+                                                ""
+                          }
+                        </div>
+                      </div>
+                    }
+                    </th>                    <th>Bill Date</th>
                     <th>Inbox Method</th>
                     <th>Amount</th>
                     <th>Actions</th>
@@ -519,7 +738,7 @@ const handleAccept = async (index) => {
                        : 
                       
                 <DeclineTable filters={filters} currentPage={currentPage}
-          itemsPerPage={itemsPerPage}/>
+          itemsPerPage={itemsPerPage} />
         
         }
 
